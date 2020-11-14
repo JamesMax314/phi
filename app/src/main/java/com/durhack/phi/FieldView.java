08a50f894c366b23651d2681a85c01e5aadcf6d2
James Maxwell <10james.maxwell@gmail.com>
2020-11-14 15:27:33 +0000

    working

--- app/src/main/java/com/durhack/phi/FieldView.java
+++ app/src/main/java/com/durhack/phi/FieldView.java
@@ -2,19 +2,20 @@ package com.durhack.phi;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
 
 public class FieldView extends View {
-    // TODO: 14/11/20 Create display canvas 
+    // TODO: 14/11/20 Create display canvas
 
-    public EditImageView(Context context) {
+    public FieldView(Context context) {
         super(context);
     }
 
-    public EditImageView(Context context, AttributeSet attributeSet) {
+    public FieldView(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
-        dpi = context.getResources().getDisplayMetrics().density;
-//        detector = new ScaleGestureDetector(context, new scaleListener());
         setFocusable(true);
     }
+
+
 
 }
