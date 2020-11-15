850cf6ea185bbbfb54ffabf0f8830430da80c98d
Daniel Nagy <daniel.nagy@durham.ac.uk>
2020-11-15 03:48:49 +0000

    Added detection class

--- app/src/main/java/com/durhack/phi/Detection.java
+++ app/src/main/java/com/durhack/phi/Detection.java
@@ -1,81 +1,100 @@
 package com.durhack.phi;
 
-import android.app.Activity;
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.os.Bundle;
+import android.os.Handler;
 
-public class Detection implements SensorEventListener{
-    // TODO: 14/11/20 Seamus compass stuff
-    private SensorManager sensorManager;
-    private Sensor sensor;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
-    public Detection(Context context){
-        sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD_UNCALIBRATED);
+public class Detection implements SensorEventListener {
+    private final int batchSize = 100;
+    private final int samplingTimeMilliseconds = 3000;
+
+    private SensorManager mSensorManager;
+
+    private final int proximityType = Sensor.TYPE_PROXIMITY;
+    private final int magnetometerType = Sensor.TYPE_MAGNETIC_FIELD_UNCALIBRATED;
+
+    private Sensor mSensorProximity;
+    private Sensor mSensorMagnetometer;
+
+    private List<Float> positionBatch;
+    private List<Float> magnetometerBatch;
+
+    public Detection(Context context) {
+        mSensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        mSensorProximity = mSensorManager.getDefaultSensor(proximityType);
+        mSensorMagnetometer = mSensorManager.getDefaultSensor(magnetometerType);
+        positionBatch = new ArrayList<>();
+        magnetometerBatch = new ArrayList<>();
     }
 
-    // TODO: 14/11/2020 Write time loop for collecting data (APi for timing)
-    //  Every x milliseconds record relative position and magnetic field
-    //  Once collected n samples return data
+    public float[] collectBatch() {
+        if (mSensorMagnetometer != null) {
+            mSensorManager.registerListener(this, mSensorMagnetometer, samplingTimeMilliseconds*1000/batchSize);
+        }
+        if (mSensorProximity != null) {
+            mSensorManager.registerListener(this, mSensorProximity, samplingTimeMilliseconds*1000/batchSize);
+        }
 
-//    public CollectData(){
-//
-//    }
+        final SensorEventListener listener = this;
+        AtomicBoolean collectionStopped = new AtomicBoolean(false);
+        new Handler().postDelayed(() -> {
+            mSensorManager.unregisterListener(listener);
+            collectionStopped.set(true);
+        }, samplingTimeMilliseconds);
+
+        while (!collectionStopped.get()) {
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException ex) {
+                // do nothing
+            }
+        }
+
+        float[] batch = new float[6*batchSize];
+        for (int i=0; i<3*batchSize; i++) {
+            batch[i] = positionBatch.get(i);
+            batch[i+3*batchSize] = magnetometerBatch.get(i);
+        }
+
+        return batch;
+    }
+
+    public void unregisterAllListeners() {
+        mSensorManager.unregisterListener(this);
+    }
 
     @Override
-    public final void onSensorChanged(SensorEvent event) {
-        // The light sensor returns a single value.
-        // Many sensors return 3 values, one for each axis.
-        float[] bField = event.values;
-        // Do something with this sensor value.
+    public void onSensorChanged(SensorEvent event) {
+        int sensorType = event.sensor.getType();
+        switch (sensorType) {
+            case proximityType:
+                if (positionBatch.size() < 3*batchSize) {
+                    positionBatch.add((float) 0);
+                    positionBatch.add((float) 0);
+                    positionBatch.add(event.values[0]);
+                }
+                break;
+            case magnetometerType:
+                if (magnetometerBatch.size() < 3*batchSize) {
+                    magnetometerBatch.add(event.values[0]);
+                    magnetometerBatch.add(event.values[1]);
+                    magnetometerBatch.add(event.values[2]);
+                }
+                break;
+            default:
+                // do nothing
+        }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
+        // Do nothing
     }
-
 }
-
-//public class SensorActivity extends Activity implements SensorEventListener {
-//    private SensorManager sensorManager;
-//    private Sensor mLight;
-//
-//    @Override
-//    public final void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-////        setContentView(R.layout.main);
-//
-//        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-//        mLight = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-//    }
-//
-//    @Override
-//    public final void onAccuracyChanged(Sensor sensor, int accuracy) {
-//        // Do something here if sensor accuracy changes.
-//    }
-//
-//    @Override
-//    public final void onSensorChanged(SensorEvent event) {
-//        // The light sensor returns a single value.
-//        // Many sensors return 3 values, one for each axis.
-//        float[] bField = event.values;
-//        // Do something with this sensor value.
-//    }
-//
-//    @Override
-//    protected void onResume() {
-//        super.onResume();
-//        sensorManager.registerListener(this, mLight, SensorManager.SENSOR_DELAY_NORMAL);
-//    }
-//
-//    @Override
-//    protected void onPause() {
-//        super.onPause();
-//        sensorManager.unregisterListener(this);
-//    }
-//}
